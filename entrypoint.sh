aaf2c915088642bebe30d359c9454d298bdf96e9
Stanislav Serebrennikov <goodsmileduck@gmail.com>
2019-10-02 17:42:17 +1300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,5 +39,5 @@ cat ~/.aws/credentials
 zip -r latest.zip ${SOURCE_DIR} 
 aws --endpoint-url=https://storage.yandexcloud.net s3 cp latest.zip s3://${BUCKET}/${FUNCTION_NAME}/latest.zip
 yc config set token ${TOKEN}
-yc serverless function version create --function-name ${FUNCTION_NAME}\
-   --package-bucket-name ${BUCKET} --package-object-name latest.zip
+yc serverless function version create --function-name ${FUNCTION_NAME} --cloud-id ${CLOUD_ID}\
+   --folder-id ${FOLDER_ID} --package-bucket-name ${BUCKET} --package-object-name latest.zip
