18850d4c65be2df6a8632a42134df5b8573d0c18
Stanislav Serebrennikov <goodsmileduck@gmail.com>
2019-10-02 14:44:57 +1300

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,8 @@
 set -e
 
 envsubst < /root/.config/yandex-cloud/config.tmpl > /root/.config/yandex-cloud/config.yaml
-yc 
+envsubst < /root/.aws/credentials.tmpl > /root/.aws/credentials
+
+zip -r latest.zip ${SOURCE_DIR} 
+aws s3 latest.zip ${S3_BUCKET}/${FUNCTION_NAME}/latest.zip
 yc serverless function version create --function-id ${FUNCTION_ID} --package-bucket-name ${S3BUCKET} --package-object-name ${S3FILE}
