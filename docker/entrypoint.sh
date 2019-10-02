d4e583beccfa6e2abca8ccb58555972d19846598
Stanislav Serebrennikov <goodsmileduck@gmail.com>
2019-10-02 14:48:30 +1300

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,28 @@
 set -e
 
+if [ -z "$BUCKET" ]; then
+  echo "AWS_S3_BUCKET is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
+  exit 1
+fi
+
+# Default to syncing entire repo if SOURCE_DIR not set.
+if [ -z "$SOURCE_DIR" ]; then
+  SOURCE_DIR="."
+fi
+
 envsubst < /root/.config/yandex-cloud/config.tmpl > /root/.config/yandex-cloud/config.yaml
 envsubst < /root/.aws/credentials.tmpl > /root/.aws/credentials
 
 zip -r latest.zip ${SOURCE_DIR} 
-aws s3 latest.zip ${S3_BUCKET}/${FUNCTION_NAME}/latest.zip
+aws --endpoint-url=https://storage.yandexcloud.net s3 latest.zip ${BUCKET}/${FUNCTION_NAME}/latest.zip
 yc serverless function version create --function-id ${FUNCTION_ID} --package-bucket-name ${S3BUCKET} --package-object-name ${S3FILE}
